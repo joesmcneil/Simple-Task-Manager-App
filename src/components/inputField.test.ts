cc66fa38b95de5be89fd2818729fbc0becc54898
UP2020033 <72713194+UP2020033@users.noreply.github.com>
2023-08-31 14:32:57 +0100

    Switched to Vitest and added a sample test

--- src/components/inputField.test.ts
+++ src/components/inputField.test.ts
@@ -1,2 +1,9 @@
 import { mount } from '@vue/test-utils';
 import inputField from '@/components/inputField.vue'; // Adjust the path as needed
+
+import { expect, test } from 'vitest'
+
+test('adds 1 + 2 to equal 3', () => {
+  const sum:number = 1
+  expect(1 + 2).toBe(3)
+})
